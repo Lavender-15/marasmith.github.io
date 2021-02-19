8effa9ef37fff79cc7d6e6e16c41fa44e0aef732
Lavender-15 <mara.lavender.smith@gmail.com>
2021-02-19 15:47:22 -0700

    lazyloading fix

--- lesson7/js/lazyloading.js
+++ lesson7/js/lazyloading.js
@@ -1,4 +1,48 @@
-//get all imgs with data-src attribute
+// get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px 50px 0px'
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+// first check to see if Intersection Observer is supported
+if ('IntersectionObserver' in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);// add , imgOptions after the curly brackets to use imgOptions
+
+  // loop through each img and check status and load if necessary
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  //just load All images if not supported
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+
+
+
+
+
+
+/*//get all imgs with data-src attribute
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //optional parameters being set for the Intersectional Observer
@@ -37,3 +81,4 @@ else {
         loadImages(img);
     });
 }
+*/
