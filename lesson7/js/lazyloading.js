638ad04ef00f84d11f0081c9121d450b6a613ca3
Lavender-15 <mara.lavender.smith@gmail.com>
2021-02-19 14:55:44 -0700

    adding lesson 7

---
+++ lesson7/js/lazyloading.js
@@ -0,0 +1,39 @@
+//get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//optional parameters being set for the Intersectional Observer
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+
+};
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {image.removeAttribute("data-src");};
+
+};
+
+//first check to see if Intersection Observer is supported
+if("IntersectionObserver" in window) { 
+    const imgObserver = new IntersectionObserver ((sites,Observer) => {
+        DataTransferItemList.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                Observer.unobserve(item.target);
+            }
+
+        });
+    }, imgOptions);
+
+    //loop through each img and check status and load if necessary
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else {
+    //just load ALL images if not suported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
