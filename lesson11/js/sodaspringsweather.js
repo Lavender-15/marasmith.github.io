23516618205237ebc625c6975a0818bac60da361
Lavender-15 <mara.lavender.smith@gmail.com>
2021-03-19 14:29:12 -0600

    fixing weathersum

--- lesson11/js/sodaspringsweather.js
+++ lesson11/js/sodaspringsweather.js
@@ -60,32 +60,39 @@ fetch(apifURL)
       }
     }
   });
-
-const apiURL =
-  "//api.openweathermap.org/data/2.5/weather?id=5607916&appid=cd74f180fdef1030c4c735b5fcd2b152&units=imperial";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((weatherInfo) => {
-    document.getElementById("speed").innerHTML = weatherInfo.wind.speed;
-    document.getElementById("humid").innerHTML = weatherInfo.main.humidity;
-    document.getElementById("hightemp").innerHTML = weatherInfo.main.temp_max;
-    let current = `${weatherInfo.weather[0].description}, ${weatherInfo.main.temp}`;
-    document.getElementById("current").innerHTML = current.replace(/^\w/, (c) =>
-      c.toUpperCase()
-    );
-
-    /*s = wind speed t = temperature*/
-    const s = weatherInfo.wind.speed;
-    const t = weatherInfo.main.temp;
-    let wc =
-      35.74 +
-      0.6215 * t -
-      35.75 * Math.pow(s, 0.16) +
-      0.4275 * t * Math.pow(s, 0.16);
-    wc = Math.round(wc);
-    if (t <= 50 && s > 3) {
-      document.getElementById("chill").textContent = wc + "\xB0" + "F";
-    } else {
-      document.getElementById("chill").textContent = "N/A";
-    }
-  });
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=cd74f180fdef1030c4c735b5fcd2b152&units=imperial";
+  fetch(apiURL)
+   .then((response) => response.json())
+   .then((weatherinfo) => {
+     //Once it comes back, display it to the console.
+     console.log(weatherinfo);
+     
+     document.getElementById('place').innerHTML=weatherinfo.name;
+     let current = `${weatherinfo.weather[0].description}, ${weatherinfo.main.temp}`;
+     document.getElementById("current").innerHTML = current.replace(/^\w/, (c) =>
+       c.toUpperCase()
+     );
+     document.getElementById('highTemp').innerHTML=weatherinfo.main.temp_max;
+     document.getElementById('windSpeed').innerHTML=weatherinfo.wind.speed;
+     document.getElementById("humid").innerHTML = weatherinfo.main.humidity;
+  ;
+ 
+  /*s = wind speed t = temperature*/
+  const s = weatherinfo.wind.speed;
+  const t = weatherinfo.main.temp;
+  let wc =
+    35.74 +
+    0.6215 * t -
+    35.75 * Math.pow(s, 0.16) +
+    0.4275 * t * Math.pow(s, 0.16);
+  wc = Math.round(wc);
+  if (t <= 50 && s > 3) {
+    document.getElementById("chill").textContent = wc + "\xB0" + "F";
+  } else {
+    document.getElementById("chill").textContent = "N/A";
+  }
+ 
+ });
+ 
+ 
+ 
